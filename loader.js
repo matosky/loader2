5ff0048d210d61503880ef61ce3f7b0e11181bb3
cyphercode <97557732+matosky@users.noreply.github.com>
2022-06-22 04:53:16 -0700

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,26 @@
+
+function initLoader(){
+  const app = document.querySelector('#header');
+  const loader = document.getElementById('loader')
+  app.style.visibility = "hidden"
+  loader.style.visibility = "visible"
+}
+
+function initApp(){
+  const app = document.querySelector('#header');
+  const loader = document.getElementById('loader')
+  app.style.visibility = "visible"
+  loader.style.visibility = "hidden"
+}
+
+
+
+
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === 'interactive') {
+    initLoader();
+  }
+  else if (event.target.readyState === 'complete') {
+    initApp();
+  }
+});
